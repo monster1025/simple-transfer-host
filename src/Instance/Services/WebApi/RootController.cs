29612d2b83d35b9fe3c6e935950eef69c9b41e22
AgentFire <agentfire@mail.ru>
2021-08-03 16:47:20 +0300

    Size Limit ++

--- src/Instance/Services/WebApi/RootController.cs
+++ src/Instance/Services/WebApi/RootController.cs
@@ -60,6 +60,7 @@ namespace SimpleTransferHost.Instance.Services.WebApi
         }
 
         [HttpPost]
+        [RequestSizeLimit(long.MaxValue)]
         public async Task<StatusCodeResult> Post(Guid serverKey, CancellationToken ct)
         {
             if (serverKey != _serverKey)
